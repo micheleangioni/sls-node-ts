fb21ce8a698df00c163a86293652dcbbb822b1a9
Michele Angioni <michele.angioni@takeaway.com>
2019-09-22 19:43:22 +0200

    Updated Handler to load repos, updated hello function to return the number of Users in the db

--- handler.ts
+++ handler.ts
@@ -1,22 +1,37 @@
 import { ApolloServer, gql } from 'apollo-server-lambda';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import infra from './src/infrastructure';
 import resolversCreator from './src/resolvers';
 
-const typeDefs = gql``;
+const { repos } = infra();
+
+const typeDefs = gql`
+  type User {
+    _id: String
+    email: String!
+    username: String!
+  }
+  type Query {
+    getUsers: [User]
+  }
+`;
 
 const resolvers = resolversCreator();
 
+// @ts-ignore
 const server = new ApolloServer({
   resolvers,
   typeDefs,
 });
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  const allUsers = await repos.user.all();
+
   return {
     body: JSON.stringify({
       input: event,
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: `Hello! ${allUsers.length} users are present in the Database`,
     }, null, 2),
     statusCode: 200,
   };
