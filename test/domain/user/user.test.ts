0b0e4f31303b39fc4598982e7d32441559efdb52
Michele Angioni <michele.angioni@takeaway.com>
2019-09-25 22:15:08 +0200

    Added Jest configuration and User Entity test, added Serverless to dev dependencies

---
+++ test/domain/user/user.test.ts
@@ -0,0 +1,29 @@
+import moment from 'moment-timezone';
+import {Moment} from 'moment-timezone/moment-timezone';
+import User from '../../../src/domain/user/user';
+
+describe('Test the User Entity', () => {
+  describe('Test instantiation', () => {
+    test('It gets correctly instantiated', async (done) => {
+      const createdAt = moment('2019-09-25T20:50:00.302Z').toDate();
+      const email = 'test@test.com';
+      const updatedAt = moment('2019-09-25T22:00:00.143Z').toDate();
+      const username = 'Test';
+
+      const userData = {
+        createdAt,
+        email,
+        updatedAt,
+        username,
+      };
+
+      const user = new User(userData);
+
+      expect((user.createdAt as Moment).toISOString()).toBe(createdAt.toISOString());
+      expect(user.email).toBe(email);
+      expect((user.updatedAt as Moment).toISOString()).toBe(updatedAt.toISOString());
+      expect(user.username).toBe(username);
+      done();
+    });
+  });
+});
