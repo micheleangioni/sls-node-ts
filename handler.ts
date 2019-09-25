e31bed57dd5ba90486f985e5e091f700f0157833
Michele Angioni <michele.angioni@takeaway.com>
2019-09-25 19:08:20 +0200

    Added error formatting, removed unused import

--- handler.ts
+++ handler.ts
@@ -1,13 +1,33 @@
 import { ApolloServer } from 'apollo-server-lambda';
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
 import schemaCreator from './src/api';
 import UserService from './src/application/user/userService';
 import { userRepo } from './src/infrastructure';
 
 const schema = schemaCreator({ userService: new UserService(userRepo) });
 
-const server = new ApolloServer({schema});
+const server = new ApolloServer({
+  formatError: (err) => {
+    // Custom errors should have a custom statusCode property < 500
+    if (err.extensions && err.extensions.exception && err.extensions.exception.statusCode < 500) {
+      console.log(JSON.stringify(err));
+    } else {
+      console.error(JSON.stringify(err));
+    }
+
+    // Return a cleaned error
+    return {
+      code: err.extensions && err.extensions.exception && err.extensions.exception.code
+        ? err.extensions.exception.code
+        : undefined,
+      message: err.message,
+      statusCode: err.extensions && err.extensions.exception && err.extensions.exception.statusCode
+        ? err.extensions.exception.statusCode
+        : undefined,
+    };
+  },
+  schema,
+});
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
   const allUsers = await userRepo.all();
