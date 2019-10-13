c11419823640d2a36a3fc90ecf2deef5fdbd5bf2
Michele Angioni <michele.angioni@takeaway.com>
2019-10-13 17:14:46 +0200

    Tweaks to Authorizer function

--- src/api/authorizer/index.ts
+++ src/api/authorizer/index.ts
@@ -2,6 +2,13 @@ import { CustomAuthorizerEvent } from 'aws-lambda';
 import { Dictionary } from '../../domain/declarations';
 import policyGenerator from './policyGenerator';
 
+// tslint:disable:max-line-length
+
+/**
+ * When calling the Callback with an error, only 'Allow', 'Deny' and 'Unauthorized' are valid values.
+ *
+ * @see https://stackoverflow.com/questions/55064760/lambda-authorizer-not-returning-proper-error-message-with-callback-in-node-js
+ */
 export default function (
     event: CustomAuthorizerEvent,
     _context: any,
@@ -21,12 +28,13 @@ export default function (
     // [...] check Authorization token
 
     // const sub = decodedToken.sub;
-    const sub = '100';
+    const sub = 100;
 
     // Add custom context to the context the Lambdas will receive upon authentication
-    const custumContext = {
-        userId: sub,
+    // All context fields MUST be strings
+    const customContext = {
+        userId: sub.toString(),
     };
 
-    return callback(null, policyGenerator(sub, 'Allow', event.methodArn, custumContext));
+    return callback(null, policyGenerator(sub.toString(), 'Allow', event.methodArn, customContext));
 }
