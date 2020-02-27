0f229eb2ab70c18e3f0b187aa65e51aca6ba8072
Michele Angioni <michele.angioni@takeaway.com>
2020-02-27 19:59:45 +0100

    Fixed bug when importing the Logger with the serverless-offline plugin

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -3,13 +3,9 @@ import userRepoCreator from './repos/userRepo';
 import brokerFactory from '@micheleangioni/node-messagebrokers';
 import config from '../config/index';
 import mongoInitializer from './mongo';
+import Logger from './logger';
 
 export default async () => {
-  // The logger could need the secrets to be loaded
-  // Require is used as dynamic imports is currently a Stage 4 (i.e. finished) proposal
-  // https://github.com/tc39/proposals/blob/master/finished-proposals.md
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const Logger = require('./logger').default;
   const logger = new Logger();
 
   const { User } = await mongoInitializer();
